f07147cbcdb4e135fb47f3fe54d34c5c0146b5ac
William Drescher <wilkua2@gmail.com>
2021-11-09 16:57:43 -0500

    preliminary command line parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use clap::{App, Arg, SubCommand};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("Tasker")
+        .version("0.1.0")
+        .author("William Drescher <wilkua2@gmail.com")
+        .about("A simple to use task management system.")
+        .subcommand(SubCommand::with_name("add")
+            .arg(Arg::with_name("params")
+                 .multiple(true)))
+        .get_matches();
+
+    match matches.subcommand() {
+        ("add", Some(sub_matches)) => println!("sub matches: {:?}", sub_matches),
+        _ => println!("something else:"),
+    };
 }
